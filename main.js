7f4691cd76a8e4a03cd3cf8d8244eb1a4b9e0cc7
Fabian <murillo.a.fabian@gmail.com>
2018-11-02 21:39:09 -0400

    completed playRound funct, game fully functional

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ function computerPlay () {
 }
 
 // Edit any user input to Capitalized first letter:
-// function capitalize(str) {
-//     let string = str.split("")
-//     for (let i=0; i<= string.length - 1; i++) {
-//         if (i === 0) {
-//             string[i] = string[i].toUpperCase();
-//         } else {
-//             string[i] = string[i].toLowerCase();
-//         }
-//     }
-//     string = string.join("");
-//     return string;
-// }
+function capitalize(str) {
+    let string = str.split("")
+    for (let i=0; i<= string.length - 1; i++) {
+        if (i === 0) {
+            string[i] = string[i].toUpperCase();
+        } else {
+            string[i] = string[i].toLowerCase();
+        }
+    }
+    string = string.join("");
+    return string;
+}
 
 
 
@@ -32,17 +32,17 @@ function game() {
         // let compScore = 0;
         // let playerScore = 0;
         // Prompt user for input:
-        let playerSel = window.prompt("Rock, Paper, Scissors?");
+        let playerSel = capitalize(window.prompt("Rock, Paper, Scissors?"));
         // Create computer selection:
         let compSel = computerPlay();
         // Play a round:
         function playRound(playerSel, compSel) {
             // If player selects Rock:
             if (playerSel === 'Rock' && compSel === 'Scissors') {
-                console.log('You win! Rock beats Scissors!');
+                console.log('You win this round! Rock beats Scissors!');
                 return 'Player won';
             } else if (playerSel === 'Rock' && compSel === 'Paper') {
-                console.log('You lose! Paper beats Rock!');
+                console.log('You lose this round! Paper beats Rock!');
                 return 'Comp won';
             } else if (playerSel === 'Rock' && compSel === 'Rock') {
                 console.log("It's a draw! Play again!");
@@ -51,10 +51,10 @@ function game() {
         
             // If player selects Paper:
             if (playerSel === 'Paper' && compSel === 'Scissors') {
-                console.log('You lose! Scissors beats Paper!');
+                console.log('You lose this round! Scissors beats Paper!');
                 return 'Comp won';
             } else if (playerSel === 'Paper' && compSel === 'Rock') {
-                console.log('You win! Paper beats Rock!');
+                console.log('You win this round! Paper beats Rock!');
                 return 'Player won';
             } else if (playerSel === 'Paper' && compSel === 'Paper') {
                 console.log("It's a draw! Play again!");
@@ -63,31 +63,33 @@ function game() {
         
             // If player selects Scissors:
             if (playerSel === 'Scissors' && compSel === 'Paper') {
-                console.log('You win! Scissors beats Paper!');
+                console.log('You win this round! Scissors beats Paper!');
                 return 'Player won';
             } else if (playerSel === 'Scissors' && compSel === 'Rock') {
-                console.log('You lose! Rock beats Scissors!');
+                console.log('You lose this round! Rock beats Scissors!');
                 return 'Comp won';
             } else if (playerSel === 'Scissors' && compSel === 'Scissors') {
                 console.log("It's a draw! Play again!");
                 return 'Draw'
             }
         }
-        playRound(playerSel, compSel)
-        if ('Player won') {
-            console.log('inside Player won');
+
+        let matchResults = playRound(playerSel, compSel);
+
+        if (matchResults === 'Player won') {
             playerScore += 1;
-        } else if ('Comp won') {
-            console.log('inside Comp won')
+        } else if (matchResults === 'Comp won') {
             compScore += 1;
-        } else {
-            playerScore += 0;
-            compScore += 0;
         }
+         
         round += 1;
         
     }
-    console.log(compScore, playerScore, round);
+    if (playerScore > compScore) {
+        console.log('You won!');
+    } else if (playerScore < compScore) {
+        console.log('Better luck next time, you lost!');
+    }
 }
 
 game();
